96aaa89a534514fcda08b153584e37e1466a1f45
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2023-08-09 15:35:41 -0400

    added links and pages for signin and signup

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,16 +7,16 @@ export function NavBar() {
     return(
         <nav className="background border-b p-4">
             <div className='flex flex-wrap items-center justify-between'>
-                <a href="https://flowbite.com/" className="flex items-center">
+                <Link href="/" className="flex items-center">
                     <Image src="https://flowbite.com/docs/images/logo.svg" width={'30'} height={'30'} className="h-8 mr-3" alt="Flowbite Logo" />
                     <span className="self-center text-2xl whitespace-nowrap ">OutputRepo</span>
-                </a>
+                </Link>
                 <div className='flex flex-wrap items-center gap-4'>
-                    <Link href={'/'}>
-                        <Button>Sign Up</Button>
+                    <Link href={'/signup'}>
+                        <Button>Sign up</Button>
                     </Link>
-                    <Link href={'/'}>
-                        <Button variant={"secondary"}>Log In</Button>
+                    <Link href={'/signin'}>
+                        <Button variant={"secondary"}>Sign in</Button>
                     </Link>
 
                     <ModeToggle/>
